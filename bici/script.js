0a38e4d0f5ae71d7730214e57fbb98ffc5a61289
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-16 14:32:41 +0100

    [array_bici]

--- bici/script.js
+++ bici/script.js
@@ -2,3 +2,28 @@
 // Creare un array di oggetti: 
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 // Stampare a schermo la bici con peso minore.
+
+// array bici
+const bikes = [
+    {nome: 'bici 1', peso: 500},
+    {nome: 'bici 2', peso: 10},
+    {nome: 'bici 3', peso: 30},
+    {nome: 'bici 4', peso: 55},
+    {nome: 'bici 5', peso: 60},
+    {nome: 'bici 6', peso: 90},
+    {nome: 'bici 7', peso: 200},
+    {nome: 'bici 8', peso: 80},
+    {nome: 'bici 9', peso: 75},
+    {nome: 'bici 10', peso: 98},
+    {nome: 'bici 11', peso: 32},
+    {nome: 'bici 12', peso: 20},
+];
+
+console.table(bikes);
+
+
+// variabile peso
+
+// ciclo
+
+
