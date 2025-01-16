3008914f0d2ee800aabc3645d46d2e2c24a2bd18
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-16 16:07:11 +0100

    [array con objects_calcio]

--- calcio/script.js
+++ calcio/script.js
@@ -5,3 +5,21 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// array con objects
+const squad = [
+    {nome: 'Frascati', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Capoterra', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Cagliari', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Rocca Priora', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Buero', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Cocciano', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Monteporzio', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Parioli', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Roma', puntifatti: 0, fallisubiti: 0},
+    {nome: 'Frosinone', puntifatti: 0, fallisubiti: 0},
+];
+
+console.table(squad);
+
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”
